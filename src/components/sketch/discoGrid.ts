3edca7e86bd6babfc6f1b4c39bacfd1be946e439
Yoon Jeong Ho <yoonjeongho33@gmail.com>
2026-09-01 14:01:25 +0900

    rewrite discoGrid

--- src/components/sketch/discoGrid.ts
+++ src/components/sketch/discoGrid.ts
@@ -9,40 +9,36 @@ export default function discoGrid(inst: p5) {
     let cols: number;
     let rows: number;
     let size: number = 10;
-    let noiseMap: number[][] = [];
     let xoff = 0;
     let yoff = 0;
     let zoff = 0;
-    let inc = 0.1;
+    let inc = 0.025;
 
     p.setup = () => {
         p.createCanvas(600, 600);
         cols = p.width / size;
         rows = p.height / size;
         p.rectMode(p.CENTER);
+        p.noStroke();
+        p.colorMode(p.HSB, 360, 100, 100)
 
     }
 
     p.draw = () => {
-        p.background(220);
+        p.background(0);
         xoff = 0;
         for (let i = 0; i < cols; i++) {
-            sizes[i] = [];
             yoff = 0;
             for (let j = 0; j < rows; j++) {
-                sizes[i][j] = p.map(p.noise(xoff, yoff, zoff), 0, 1, 0, size * 1.5);
+                let n = p.noise(xoff, yoff, zoff);
+                let s = p.map(n, 0, 1, -size, size * 1.5);
+                let c = p.map(p.noise(xoff + 100, yoff + 100, zoff + 100), 0, 1, 0, 360);
+                p.fill(c, 80, 90);
+                p.rect(i * size + size / 2, j * size + size / 2, s, s);
                 yoff += inc;
-
-                let r = p.noise(zoff) * 255;
-                let g = p.noise(zoff + 10) * 255;
-                let b = p.noise(zoff + 20) * 255;
-
-                p.fill(r, g, b);
-                p.noStroke();
-                p.rect(i * size + size / 2, j * size + size / 2, sizes[i][j], sizes[i][j]);
             }
             xoff += inc;
-            zoff += 0.00035;
         }
+        zoff += inc;
     }
 }
