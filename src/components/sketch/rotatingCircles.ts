157b21c479ada828a1edefc20447a477ba5dc4e3
Yoon Jeong Ho <yoonjeongho33@gmail.com>
2026-09-02 19:59:40 +0900

    .

--- src/components/sketch/rotatingCircles.ts
+++ src/components/sketch/rotatingCircles.ts
@@ -7,7 +7,6 @@ export default function rotatingCircles(inst: p5) {
 
     let circles1: Pack;
     let circles2: Pack;
-    let speed = 1;
 
     p.setup = () => {
         p.createCanvas(600, 600);
@@ -18,57 +17,56 @@ export default function rotatingCircles(inst: p5) {
 
     p.draw = () => {
         p.background(0, 10);
-        circles1.displayPack();
-        circles1.movePack(speed);
-        circles2.displayPack();
-        circles2.movePack(speed);
-    };
+        circles1.update(1);
+        circles1.display();
+        circles2.update(1);
+        circles2.display();
+    }
 }
 
 class Pack {
-    packx: number;
-    packy: number;
-    pack: Circle[];
+    x: number;
+    y: number;
     n: number;
+    r: number;
+    size: number;
     angle: number;
-    packr: number;
     startAngle: number;
+    circles: Circle[];
 
     constructor(x: number, y: number, n: number, size: number, startAngle: number) {
-        this.packx = x;
-        this.packy = y;
-        this.pack = [];
-
+        this.x = x;
+        this.y = y;
         this.n = n;
+        this.r = 150;
+        this.size = size;
         this.angle = size / this.n;
-        this.packr = 150;
         this.startAngle = startAngle;
+        this.circles = [];
+
         this.createCircles();
     }
 
     createCircles() {
-        let scl = 0.1;
         for (let i = 0; i < this.n; i++) {
-            this.pack[i] = new Circle(
-                this.packx + this.packr * p.cos(this.startAngle + this.angle * i),
-                this.packy + this.packr * p.sin(this.startAngle + this.angle * i),
-                (i + 1) * scl * 25
-            );
+            let x = this.x + this.r * p.cos(this.angle * i + this.startAngle);
+            let y = this.y + this.r * p.sin(this.angle * i + this.startAngle);
+            this.circles[i] = new Circle(x, y, 10);
         }
     }
 
-    displayPack() {
-        for (let i = 0; i < this.n; i++) {
-            this.pack[i].display();
-            this.pack[i].move();
-        }
-    }
-
-    movePack(speed: number) {
+    update(speed: number) {
         this.startAngle += speed;
         for (let i = 0; i < this.n; i++) {
-            this.pack[i].x = this.packx + this.packr * p.cos(this.startAngle + this.angle * i);
-            this.pack[i].y = this.packy + this.packr * p.sin(this.startAngle + this.angle * i);
+            this.circles[i].x = this.x + this.r * p.cos(this.angle * i + this.startAngle);
+            this.circles[i].y = this.y + this.r * p.sin(this.angle * i + this.startAngle);
+        }
+    }
+
+    display() {
+        for (let i = 0; i < this.n; i++) {
+            this.circles[i].update();
+            this.circles[i].display();
         }
     }
 }
@@ -77,25 +75,25 @@ class Circle {
     x: number;
     y: number;
     r: number;
-    dr: number;
+    dir: number;
 
     constructor(x: number, y: number, r: number) {
         this.x = x;
         this.y = y;
         this.r = r;
-        this.dr = 1;
+        this.dir = 1;
+    }
+
+    update() {
+        if (this.r > 50 || this.r < 0) {
+            this.dir = -this.dir;
+        }
+        this.r += this.dir;
     }
 
     display() {
         p.noFill();
         p.stroke(255);
         p.ellipse(this.x, this.y, this.r * 2, this.r * 2);
-    }
-
-    move() {
-        if (this.r > 50 || this.r < 0) {
-            this.dr = this.dr * -1;
-        }
-        this.r = this.r + this.dr;
     }
 }
